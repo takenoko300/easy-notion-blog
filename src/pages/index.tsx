1702b8f7f3bfdd68a61058f3afbbae6115fb216b
takenoko300 <takeyoko1115@yahoo.co.jp>
2022-07-07 22:10:29 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,23 @@ const RenderPage = () => (
       {/* <h1>techPeck</h1> */}
 
       <h2>About</h2>
-      <p>Your easy-notion-blog deployed successfully!</p>
-      <p>Have fun!</p>
-      <p>
+      <p>機械、電気、プログラミングなどで遊び、学んだ内容を発信します。<br></br>
+      ただし、今はp5.jsにハマっているため、その内容ばかり。</p>
+    </div>
+
+    <div>
+      <h2>Profile</h2>
+      <p>メーカーに勤務する機械系エンジニアです。<br></br>
+      プログラミングは趣味ですが、副業できるレベルを目指しています。</p>
+
+      {/* hide 20220707 */}
+      {/* <p>
         easy-notion-blog powered by{' '}
         <ExtLink href="https://github.com/otoyo/easy-notion-blog">
           otoyo/easy-notion-blog
         </ExtLink>
-      </p>
+      </p> */}
+      {/* hide 20220707 */}
     </div>
   </div>
 )
