637545e9e8eeec84ad6687c48790f14e885cba01
otoyo <hiroki.t.1988@gmail.com>
2022-07-18 11:50:16 +0900

    upgrade

--- src/components/notion-blocks/code.tsx
+++ src/components/notion-blocks/code.tsx
@@ -1,14 +1,28 @@
 import dynamic from 'next/dynamic'
 import Prism from 'prismjs'
-import 'prismjs/components/prism-jsx'
+
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-diff'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-elixir'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-hcl'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-yaml'
 
 import styles from '../../styles/notion-block.module.css'
 
 const Mermaid = dynamic(() => import('./mermaid'))
 
 const Code = ({ block }) => {
-  const code = block.Code.Text.map(richText => richText.Text.Content).join('')
-  const language = block.Code.Language || 'javascript'
+const code = block.Code.Text.map((richText: RichText) => richText.Text.Content).join('')
+const language = block.Code.Language.toLowerCase()
+const grammer = Prism.languages[language.toLowerCase()] || Prism.languages.javascript
 
   return (
     <div className={styles.code}>
@@ -18,11 +32,7 @@ const Code = ({ block }) => {
         <pre>
           <code
             dangerouslySetInnerHTML={{
-              __html: Prism.highlight(
-                code,
-                Prism.languages[language.toLowerCase()] ||
-                  Prism.languages.javascript
-              ),
+              __html: Prism.highlight(code, grammer, language),
             }}
           />
         </pre>
