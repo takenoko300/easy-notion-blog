a95cec585e392937d643a9b9b2dda405fd10caa6
Hiroki Toyokawa <hiroki.t.1988@gmail.com>
2022-07-18 12:54:11 +0900

    upgrade

--- src/components/notion-blocks/code.tsx
+++ src/components/notion-blocks/code.tsx
@@ -15,14 +15,28 @@ import 'prismjs/components/prism-sql'
 import 'prismjs/components/prism-typescript'
 import 'prismjs/components/prism-yaml'
 
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-diff'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-elixir'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-hcl'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-yaml'
+
 import styles from '../../styles/notion-block.module.css'
 
 const Mermaid = dynamic(() => import('./mermaid'))
 
 const Code = ({ block }) => {
-const code = block.Code.Text.map((richText: RichText) => richText.Text.Content).join('')
-const language = block.Code.Language.toLowerCase()
-const grammer = Prism.languages[language.toLowerCase()] || Prism.languages.javascript
+  const code = block.Code.Text.map((richText: RichText) => richText.Text.Content).join('')
+  const language = block.Code.Language.toLowerCase()
+  const grammer = Prism.languages[language.toLowerCase()] || Prism.languages.javascript
 
   return (
     <div className={styles.code}>
